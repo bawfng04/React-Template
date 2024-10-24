da719220e92fc2eaf1973e3f1dc27e3d70ab1c86
Bằng <133222169+bawfng04@users.noreply.github.com>
2024-10-24 09:14:48 +0700

    fix login with Google

--- src/template/Login/LoginWithGoogle.js
+++ src/template/Login/LoginWithGoogle.js
@@ -1,25 +1,26 @@
-import React from 'react';
-import { GoogleLogin, GoogleOAuthProvider } from '@react-oauth/google';
-import { useNavigate } from 'react-router-dom';
-import { clientID } from './config';
+import React from "react";
+import { GoogleLogin, GoogleOAuthProvider } from "@react-oauth/google";
+import { useNavigate } from "react-router-dom";
+import { clientID } from "./config";
 
-function LoginWithGoogle() {
-    const navigate = useNavigate();
-    const onSuccess = (response) => {
-        alert('Login with Google success');
-        navigate('/hof');
-    };
-    const onFailure = (response) => {
-        alert('Login with Google failed: ' + response.error);
-    };
+function LoginWithGoogle({ successLogin, failureLogin }) {
+  const navigate = useNavigate();
+  const onSuccess = (response) => {
+    successLogin();
+    navigate("/home");
+  };
+  const onFailure = (response) => {
+    failureLogin();
+    alert("Login with Google failed: " + response.error);
+  };
 
-    return (
-        <GoogleOAuthProvider clientId={clientID}>
-            <div className="LoginWithGoogle">
-                <GoogleLogin onSuccess={onSuccess} onError={onFailure} />
-            </div>
-        </GoogleOAuthProvider>
-    );
+  return (
+    <GoogleOAuthProvider clientId={clientID}>
+      <div className="LoginWithGoogle">
+        <GoogleLogin onSuccess={onSuccess} onError={onFailure} />
+      </div>
+    </GoogleOAuthProvider>
+  );
 }
 
 export default LoginWithGoogle;
